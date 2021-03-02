558066f51e2530d2ba4e251916192927375247b7
dam1b <dmarinoalonso@danielcastelao.org>
2021-03-02 11:00:57 +0100

    cambios

--- src/Visualizar/FactoriaDeSalida.java
+++ src/Visualizar/FactoriaDeSalida.java
@@ -2,7 +2,7 @@ package Visualizar;
 
 public class FactoriaDeSalida {
     public static final int CONSOLA = 1;
-    public static final int VENTANA = 2;
+    public static final int VENTANA = 3;
     public static class InterfaceMetodosComunes getProducto(int type, String){
 switch (type){
     case CONSOLA:
